244e8579a874068cae7bb52ae8b44b9ce7ef13ed
Yan Cota <yancota10@gmail.com>
2022-12-06 20:43:22 -0300

    Programa básico para verificar se CPF é válido

---
+++ src/calculo_Cpf/Programa.java
@@ -0,0 +1,12 @@
+package calculo_Cpf;
+import javax.swing.JOptionPane;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "O último número é: " + 1 * 2);
+		System.exit(0);
+	}
+
+}
